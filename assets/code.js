6f7eacffa562db976d6ae408ef5623512bbed261
graym <yamawarrior@gmail.com>
2014-07-08 11:00:51 -0500

    update again

--- assets/code.js
+++ assets/code.js
@@ -1,8 +1,8 @@
-$(function(){ 
+$(function() {
     $('.profile');
 
 
-var users = [
+    var users = [
         'robdesousa',
         'Keya-Moradi',
         'Matthiasak',
@@ -12,34 +12,62 @@ var users = [
         'davidjfreedman',
         'graym'
     ];
-    var random = users[Math.floor(Math.random() * users.length)];
-    var github_username = random;
-    var github_url = "https://api.github.com/users/";
-    var url = github_url + github_username;
-    var github_data = $.getJSON(url);
-    //******I want ImgBank to read the names in a column*******//
-    // var person = prompt("Select a username from the following: " +" "+ImgBank);
-    console.log(github_data);
-
-
-        function init(){
-            github_data.then(function(data){
-            var github_html = createProfileStringFromData(data);
-            $('body').append(github_html);
-        });
-
 
     var template_string = $('#github_profile_template')[0].textContent;
     var createProfileStringFromData = _.template(template_string);
 
-    var rightButton = $(".next");
-    rightButton.addEventListener("click", init);
-    console.log("button clicked"); 
-    
-    var leftButton = $(".prev");
-    leftButton.addEventListener("click", init);
-    console.log("button clicked");
+    var rightButton = document.querySelector(".next");
+    rightButton.addEventListener('click', function() {
+        console.log('???'); //partial fix #1
+        init();
+    });
 
+    // var rightMouseUp = document.querySelector(".next");
+    // rightMouseUp.addEventListener('mouseup', clear);
+
+    var leftButton = document.querySelector(".prev");
+    leftButton.addEventListener('click', function() {
+        init();
+    });
+
+    function init() {
+        // get new profile
+        // get data
+        // template
+        // ...
+
+
+        // fix #2
+        var random = users[Math.floor(Math.random() * users.length)];
+        var github_username = random;
+        var github_url = "https://api.github.com/users/";
+        var url = github_url + github_username;
+        // 
+        $.getJSON(url).then(function(data) {
+            var github_html = createProfileStringFromData(data);
+            $('.profile').remove(); // fix #1
+            $('body').append(github_html);
+        }).fail(function(promise_obj, error_type, error_message) {
+            alert("Ohhhh you've done it now. -- "+promise_obj.status+", "+error_message);
+        })
     };
+
+    // function clear() {
+    //     $('.profile').remove();
+    //     console.log("cliky");
+    // };
+
+    // function refresh() {
+    //     github_data.then(function(data) {
+    //         var github_html = createProfileStringFromData(data);
+    //         window.location.reload(false)
+    //     }).fail(function(promise_obj, error_type, error_message) {
+    //         alert("Don't touch that!!!", promise_obj.status, error_message);
+    //     })
+    // };
+
+
+
+    init();
 
 });
