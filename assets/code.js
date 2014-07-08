e20b8b106dfcf656f1fd6d0cce68fd6502611c05
graym <yamawarrior@gmail.com>
2014-07-07 20:33:37 -0500

    update

--- assets/code.js
+++ assets/code.js
@@ -1,8 +1,8 @@
-$(function(){ 
+$(function() {
     $('.profile');
 
 
-var users = [
+    var users = [
         'robdesousa',
         'Keya-Moradi',
         'Matthiasak',
@@ -17,29 +17,29 @@ var users = [
     var github_url = "https://api.github.com/users/";
     var url = github_url + github_username;
     var github_data = $.getJSON(url);
-    //******I want ImgBank to read the names in a column*******//
-    // var person = prompt("Select a username from the following: " +" "+ImgBank);
-    console.log(github_data);
+    
+    var template_string = $('#github_profile_template')[0].textContent;
+    var createProfileStringFromData = _.template(template_string);
+    
+    var rightButton = $(".next");
+        rightButton.click(init);
+        clear;
+        console.log("button clicked");
+    var leftButton = $(".prev");
+        leftButton.click(init);
+        console.log("button clicked");
 
-
-        function init(){
-            github_data.then(function(data){
+    function init() {
+        github_data.then(function(data) {
             var github_html = createProfileStringFromData(data);
             $('body').append(github_html);
         });
-
-
-    var template_string = $('#github_profile_template')[0].textContent;
-    var createProfileStringFromData = _.template(template_string);
-
-    var rightButton = $(".next");
-    rightButton.addEventListener("click", init);
-    console.log("button clicked"); 
     
-    var leftButton = $(".prev");
-    leftButton.addEventListener("click", init);
-    console.log("button clicked");
+    }
+    function clear() {
+        $('.profile').empty();
 
-    };
+    }
+init();
 
 });
