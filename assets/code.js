19a1859dab46cc02ef83212327952fde3623e4e6
graym <yamawarrior@gmail.com>
2014-07-07 19:34:38 -0500

    fix

--- assets/code.js
+++ assets/code.js
@@ -2,7 +2,7 @@ $(function(){
     $('.profile');
 
 
-var ImgBank = [
+var users = [
         'robdesousa',
         'Keya-Moradi',
         'Matthiasak',
@@ -14,31 +14,21 @@ var ImgBank = [
     ];
     var random = users[Math.floor(Math.random() * users.length)];
     var github_username = random;
-    var github_data = $.getJSON(url);
     var github_url = "https://api.github.com/users/";
     var url = github_url + github_username;
+    var github_data = $.getJSON(url);
     //******I want ImgBank to read the names in a column*******//
     // var person = prompt("Select a username from the following: " +" "+ImgBank);
-    
+    console.log(github_data);
+
+
         function init(){
             github_data.then(function(data){
             var github_html = createProfileStringFromData(data);
             $('body').append(github_html);
-        }
+        });
 
-    // var github_username = person;
-    
 
-    //******Want alert to return "No such user" if person !== name in array********//
-    // if (typeof person ==='string') {
-        // alert("Welcome to"+" "+person+"\'s profile!");
-    //  alert("Welcome to" + " " + person + "\'s profile!")
-    // }
-    // console.log('grabbing profile data:', github_url+github_username);
-
-    
-
-    // template
     var template_string = $('#github_profile_template')[0].textContent;
     var createProfileStringFromData = _.template(template_string);
 
@@ -50,32 +40,6 @@ var ImgBank = [
     leftButton.addEventListener("click", init);
     console.log("button clicked");
 
-    // github_data.then(function(data){
-    //  var github_html = createProfileStringFromData(data);
-        // $('body').append(github_html);
-
-        // var clickRight = $('.b1');
-        // clickRight.addEventListener('click', picR);
-        // var clickLeft = $('.b2');
-        // clickLeft.addEventListener('click', picL);
-
-        // var n = 0;
-
-        // function picR(change) {
-        //  if (n < (ImgBank.length-1))
-        //      n++;
-        //  else
-        //      n = 0;
-        //  document.getElementsByClass("pic").src = data["avatar_url"];
-        // }
-        // function picL(change) {
-        //  if (n > 0)
-        //      n--;
-        //  else
-        //      n = (ImgBank.length-1);
-        //  document.getElementsByClass("pic").src = data["avatar_url"];
-        // }
-
-    });
+    };
 
 });
