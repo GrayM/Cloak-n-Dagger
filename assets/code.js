3746a9b56f9325d193c29ad0aacfde2590a6ddc5
graym <yamawarrior@gmail.com>
2014-07-07 22:00:06 -0500

    fixed js

--- assets/code.js
+++ assets/code.js
@@ -12,34 +12,44 @@ $(function() {
         'davidjfreedman',
         'graym'
     ];
+
     var random = users[Math.floor(Math.random() * users.length)];
     var github_username = random;
     var github_url = "https://api.github.com/users/";
     var url = github_url + github_username;
     var github_data = $.getJSON(url);
-    
     var template_string = $('#github_profile_template')[0].textContent;
     var createProfileStringFromData = _.template(template_string);
-    
-    var rightButton = $(".next");
-        rightButton.click(init);
-        clear;
-        console.log("button clicked");
-    var leftButton = $(".prev");
-        leftButton.click(init);
-        console.log("button clicked");
+
+    var rightButton = document.querySelector(".next");
+    rightButton.addEventListener('click', refresh);
+
+    var rightMouseUp = document.querySelector(".next");
+    rightMouseUp.addEventListener('mouseup', clear);
+
+    var leftButton = document.querySelector(".prev");
+    leftButton.addEventListener('click', refresh);
 
     function init() {
         github_data.then(function(data) {
             var github_html = createProfileStringFromData(data);
             $('body').append(github_html);
         });
-    
     }
-    function clear() {
-        $('.profile').empty();
 
+    function clear() {
+        $('.profile').remove();
+        console.log("cliky");
     }
-init();
+
+    function refresh() {
+        github_data.then(function(data) {
+            var github_html = createProfileStringFromData(data);
+            window.location.reload()
+
+        })
+    }
+
+    init();
 
 });
