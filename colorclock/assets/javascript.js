3f4bce35987dcaa86a738072a2dd24356a7aa728
Joey Bergeron <joey.bergeron@gmail.com>
2014-07-09 22:17:23 -0500

    js working now (time and background color)

--- colorclock/assets/javascript.js
+++ colorclock/assets/javascript.js
@@ -1,4 +1,4 @@
-function clock() {
+setInterval(function clock() {
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
@@ -9,29 +9,23 @@ function clock() {
     var t = setTimeout(function() {
         clock()
     }, 500);
+
+    function colorChanger() {
+        var d = today.getTime() / (1000 * 60 * 60 * 24)
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        return "rgb(" +
+            (((d % 2) == 1) ? parseInt((h / 24) * 256) : (255 - parseInt((h / 24) * 256))) + "," +
+            (((h % 2) == 1) ? parseInt((m / 60) * 256) : (255 - parseInt((m / 60) * 256))) + "," +
+            (((m % 2) == 1) ? parseInt((s / 60) * 256) : (255 - parseInt((s / 60) * 256))) + ")"
+
+    };
     $('body').css('backgroundColor', colorChanger)
-}
-
-
-var today = new Date();
-var h = today.getHours();
-var m = today.getMinutes();
-var s = today.getSeconds();
-m = checkTime(m);
-s = checkTime(s);
-
-function colorChanger() {
-    var d = today.getTime() / (1000 * 60 * 60 * 24)
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    return "rgb(" +
-        (((d % 2) == 1) ? parseInt((h / 24) * 256) : (255 - parseInt((h / 24) * 256))) + "," +
-        (((h % 2) == 1) ? parseInt((m / 60) * 256) : (255 - parseInt((m / 60) * 256))) + "," +
-        (((m % 2) == 1) ? parseInt((s / 60) * 256) : (255 - parseInt((s / 60) * 256))) + ")"
-
-};
-
+    var rgbFix = colorChanger().replace("rgb(", "").replace(")","");    
+    console.log(rgbFix);
+    $('.color').html(rgbFix)
+}, 1000);
 
 
 function checkTime(i) {
@@ -39,9 +33,4 @@ function checkTime(i) {
         i = "0" + i
     }; // add zero in front of numbers < 10
     return i;
-
 };
-
-
-clock();
-
